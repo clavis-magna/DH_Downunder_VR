3534218d32dd269e144d671df5986096485b9174
Andrew <andrew.burrell@uts.edu.au>
2019-12-06 16:39:28 +1100

    flycam features

---
+++ UnityProject/DHDownunder-genericVR/Assets/scripts/extendedFlycam.cs
@@ -0,0 +1,84 @@
+﻿/*
+Based on Desi Quintans (CowfaceGames.com) + FlyThrough.js http://wiki.unity3d.com/index.php/FlyThrough
+EXTENDED further for VR use in the Glossopticon & Layered Horizons Projects
+
+MOUSE LOOK + WASD/Arrows | Q&E climb | shift speed up | CTR slow down
+IN VR - right trackpad moves in the direction of gaze
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class extendedFlycam : MonoBehaviour
+{
+    public float cameraSensitivity = 90;
+    public float climbSpeed = 4;
+    public float normalMoveSpeed = 10;
+    public float slowMoveFactor = 0.25f;
+    public float fastMoveFactor = 3;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    public Transform trackpadIndicator;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //
+        // Mouse and Keyboard
+        //
+        if (!UnityEngine.XR.XRDevice.isPresent)
+        {
+            if (transform.position.y >= 25)
+            {
+                transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, 25, transform.position.z);
+            }
+            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+            rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            {
+                transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            }
+
+
+            if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
+            if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+        }
+        // Simple VR Navigation
+        else
+        {
+            float speedUp = helpers.Remap(Input.GetAxis("VRRightTrackVerticle"), -1, 1, -3, 3);
+            float indicatorPos = helpers.Remap(Input.GetAxis("VRRightTrackVerticle"), -1, 1, -0.4f, 0.4f);
+            trackpadIndicator.localPosition = new Vector3(trackpadIndicator.localPosition.x, trackpadIndicator.localPosition.y, indicatorPos);
+            transform.position += Camera.main.transform.forward * normalMoveSpeed * speedUp * Time.deltaTime;
+        }
+    }
+}
