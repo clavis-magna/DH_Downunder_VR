a4165326b0a3119327c65792a15d2adb1793821a
Andrew <andrew.burrell@uts.edu.au>
2019-12-06 14:43:30 +1100

    new generic project

---
+++ UnityProject/DHDownunder-genericVR/Assets/scripts/controllerButtons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class controllerButtons : MonoBehaviour
+{
+    public GameObject leftThumb;
+    public GameObject rightThumb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Input.GetAxis("VRLeftTrigger"));
+        if(Input.GetAxis("VRLeftTrigger") > 0.6f)
+        {
+            leftThumb.transform.localRotation = Quaternion.Euler(0, 0, 0); 
+        }
+        else
+        {
+            leftThumb.transform.localRotation = Quaternion.Euler(0, 45, 0);
+        }
+
+        if (Input.GetAxis("VRRightTrigger") > 0.6f)
+        {
+            rightThumb.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            rightThumb.transform.localRotation = Quaternion.Euler(0, -45, 0);
+        }
+    }
+
+
+}
