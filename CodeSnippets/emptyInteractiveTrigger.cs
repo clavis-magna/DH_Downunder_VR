5fb1d110d15f266f858f4694d29c928feb88741a
Andrew Burrell <111302@DABMLB0606621AB.local>
2019-12-07 19:23:52 +1100

    code snippets

---
+++ CodeSnippets/emptyInteractiveTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class emptyInteractiveTrigger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("enter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("exit");
+    }
+}
